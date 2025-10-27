a6c65e00151de68be9ec405f306df8030b58b273
Mateo2alejo <alejandroviracocha844@gmail.com>
2025-10-27 10:02:23 -0500

    clase 2025-10-27

---
+++ modulo2/dom/02_seleccionar_elementos.js
@@ -0,0 +1,9 @@
+const titulo = document.getElementById('titulo');
+console.log(titulo.textContent);
+const notas = document.getElementsByClassName('nota');
+Array.from(notas).forEach(nota => console.log(nota.textContent));
+const items = document.getElementsByClassName('item');
+items.forEach(elemento => console.log(elemento.textContent));
+const usuario = document.getElementsByName('usuario');
+console.log(usuario.textContent);
+Array.from(usuarios).forEach(usuario => console.log(usuario,textContent));

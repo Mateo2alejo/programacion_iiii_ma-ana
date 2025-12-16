aa1045471b11e751faaa33b5a26b1d368baa2042
Mateo2alejo <alejandroviracocha844@gmail.com>
2025-12-16 14:45:10 -0500

    clase 16-12-2025

--- Modulo3/my-react-app/src/App.tsx
+++ Modulo3/my-react-app/src/App.tsx
@@ -1,119 +1,98 @@
-{/*import ButtonCounter from './components/ButtonCounter';
-import ColorBox from './components/ColorBox';
-import HelloWorld from './components/HelloWorld';
-import ShowDate from './components/ShowDate';
-import TextInput from './components/TextInput';
-import ToggleMessage from './components/ToggleMessage';
-import WelcomeUser from './components/WelcomeUser';
-import ConditionalText from './components/ConditionalText';
-import EmojiList from './components/EmojiList';
-import SimpleForm from './components/SimpleForm';
-import SumaForm from './components/SumaForm';
+//import ButtonCounter from './components/ButtonCounter';
+//import ColorBox from './components/ColorBox';
+//import ConditionalText from './components/ConditionalText';
+//import EmojiList from './components/EmojiList';
+//import HelloWorld from './components/HelloWorld';
+//import ShowDate from './components/ShowDate';
+//import SimpleForm from './components/SimpleForm';
+//import SumaForm from './components/SumaForm';
+//import TextInput from './components/TextInput';
+//import ToggleMessage from './components/ToggleMessage';
+//import WelcomeUser from './components/WelcomeUser';
+//import PromedioTresNotas from './components/PromedioTresNotas';
+//import Greeting from './props/Greeting';
+//import UserCard from './props/UserCard';
+//import ParentButtonOne from './props/ParentButtonOne';
+//
+//function App() {
+//
+//  const userData = {
+//    name: "Francisco",
+//    age: 25,
+//    email: "francisco@example.com"
+//  };
+//
+//  return (
+//    <>
+//      <HelloWorld />
+//      <WelcomeUser />
+//      <ButtonCounter />
+//      <TextInput />
+//      <ToggleMessage />
+//      <ShowDate />
+//      <ColorBox />
+//      <ConditionalText />
+//      <EmojiList />
+//      <SimpleForm />
+//      <SumaForm />
+//      <PromedioTresNotas />
+//      <Greeting name="Francisco" />
+//      <Greeting name="Ana" />
+//      <UserCard user={userData} />
+//      <ParentButtonOne />
+//    </>
+//  );
+//}
+//
+//
+//
+//export default App;
 
 
-import Greeting from "./props/Greeting";
-import PromedioTresNotasForm from './components/PromedioTresNotasForm';
-import UserCard from './props/UserCard';
-import ParentButtonOne from './props/ParentButtonOne';
-import ParentInputNotifier from './props/ParentInputNotifier';
 
 
-import HoverFont from './useState/Hoverfont';
-import AccessWithLimit from './useState/AccessWithLimit';
-import LoginWithLimit from './useState/LoginWithLimit';
-import DocumentTitleChanger from './useState/DocumentTitleChanger';
-import PostLikes from './useState/PostLikes';
-import CheckboxSummary from './useState/CheckboxSummary';
-import LanguageSwitcher from './useState/LanguageSwitcher';
-import MultiSwitch from './useState/MultiSwitch';
-import DynamicColor from './useState/DynamicColor';
-import ClickHistory from './useState/ClickHistory';
-import LogEffect from './useEffect/LogEffect';
-import FetchUser from './useEffect/FetchUser';
-import Clock from './useEffect/Clock';
-import FetchUsers from './useEffect/FetchUsers';
-import ScrollLogger from './useEffect/ScrollLogger';
-import PersistCounter from './useEffect/PersistCounter';
+import FetchUser from "./useEffect/FetchUser";
+import LogEffect from "./useEffect/LogEffect";
+import ScrollLogger from "./useEffect/ScrollLoger";
+import CirculoColorido from "./useRef/CirculoColorido";
+import ScrollDemo from "./useRef/ScrollDemo";
+import AccessWithLimit from "./useState/AccesWithLimit";
+import CheckboxSummary from "./useState/CheckboxSummary";
+import ClickHistory from "./useState/ClickHistory";
+import DocumentTitleChanger from "./useState/DocumentTitleChanger";
+import DynamicColor from "./useState/DynamicColor";
+import Hoverfont from "./useState/Hoverfont";
+import LanguageSwitcher from "./useState/LanguageSwitcher";
+import LoginWithLimit from "./useState/LoginWithLimit";
+import MultiSwitch from "./useState/MultiSwitch";
+import PostLikes from "./useState/PostLikes";
+import ReproductorSonido from "./useRef/ReproductorSonido";
+import MoverCaja from "./useRef/MoverCaja";
 
-
-
-function App() {
+function App () {
   return (
-    <>{/*
-      <HelloWorld />
-      <WelcomeUser />
-      <ButtonCounter />
-      <ShowDate />
-      <TextInput />
-      <ToggleMessage />
-      <ColorBox />
-      <ConditionalText />
-      <EmojiList />
-      <SimpleForm />
-      <SumaForm />
+    <>
 
-      <Greeting name="Santiago" />
-      <Greeting name="Maria" />
-      <PromedioTresNotasForm />
-      <UserCard user={userData} />
-      <ParentButtonOne />
-      {/* <ParentInputNotifier /> 
-
-      <HoverFont/>
-      <AccessWithLimit/>
-      <LoginWithLimit/>
-      <DocumentTitleChanger/>
-      <PostLikes/>
-      <CheckboxSummary/>
-      <LanguageSwitcher/>
-      <MultiSwitch/>
-      <DynamicColor/>
-      <ClickHistory/>
-      <LogEffect/>
-      <FetchUser/>
-      <Clock/>
-      <FetchUsers/>
-      <ScrollLogger/>
-      <PersistCounter/>
-
+    <HoverFont />
+    <AccessWithLimit />
+    <LoginWithLimit />
+    <DocumentTitleChanger />
+    <PostLikes />
+    <CheckboxSummary />
+    <LanguageSwitcher />
+    <MultiSwitch />
+    <DynamicColor />
+    <ClickHistory />
+    <LogEffect />
+    <FetchUser />
+    <ScrollLogger />
+    <CirculoColorido />
+    <ScrollDemo />
+    <ReproductorSonido/>
+    <MoverCaja />
+    
     </>
-  );
+  )
 }
 
 export default App;
-*/}
-
-{/*
-// useContext, languajeContext, LanguageToggle
-
-import React, { useState } from 'react';
-import { LanguageContext } from './useContext/LanguageContext';
-import LanguageToggle from './useContext/LanguageToggle';
-
-export default function App() {
-  const [lang, setLang] = useState('es');
-  const toggleLanguage = () => setLang(prev => (prev === 'es' ? 'en' : 'es'));
-
-  return (
-    <LanguageContext.Provider value={{ lang, toggleLanguage }}>
-      <LanguageToggle />
-    </LanguageContext.Provider>
-  );
-}
-*/}
-
-
-import React, { useState } from 'react';
-import { LoginContext } from './useContext/LoginContext';
-import LoginStatus from './useContext/LoginStatus';
-
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const toggleLogin = () => setIsLoggedIn(prev => !prev);
-
-  return (
-    <LoginContext.Provider value={{ isLoggedIn, toggleLogin }}>
-      <LoginStatus />
-    </LoginContext.Provider>
-  );
-}
